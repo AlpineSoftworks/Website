cab03fccee3856633d85b887cef3b1533b97d8be
0boris <23bosmanov@heckgrammar.co.uk>
2025-03-06 18:33:21 +0000

    Hello, Alpine!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<div
+    class="flex flex-col h-screen w-full bg-radial-[90%_80%_at_50%_105%] from-[#7d3e1f] to-black"
+>
+    <div class="flex-grow flex flex-col justify-center">
+        <div class="flex justify-center text-white">
+            <svg
+                class="fill-orange-500 w-52"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 25 24"
+                ><path
+                    d="M7.91 5.688c.848-1.584 3.12-1.584 3.967 0l3.204 5.986l.23-.485c.815-1.71 3.249-1.71 4.063 0l2.427 5.095c.711 1.493-.377 3.217-2.031 3.217H4.273c-1.7 0-2.786-1.813-1.984-3.312zm4.644 12.313h7.216a.75.75 0 0 0 .677-1.072l-2.427-5.095a.75.75 0 0 0-1.354 0l-.87 1.824a.75.75 0 0 1-1.337.032l-3.904-7.294a.75.75 0 0 0-1.323 0l-5.62 10.501a.75.75 0 0 0 .66 1.104z"
+                    clip-rule="evenodd"
+                /></svg
+            >
+        </div>
+        <div class="text-center">
+            <h1
+                class="text-8xl font-bold pb-4 bg-gradient-to-br from-orange-400 to-red-500 inline-block text-transparent bg-clip-text"
+            >
+                Alpine
+        </div>
+    </div>
+
+    <footer
+        class="fixed bottom-0 left-1/2 transform -translate-x-1/2 text-center"
+    >
+        <span class="text-white/70"
+            >Developing cutting-edge software and websites with alpine
+            precision.</span
+        >
+        <div class="flex justify-center mt-4">
+            <a aria-label="Alpine GitHub" href="https://github.com/AlpineSoftworks" class="relative flex w-12 h-8 items-center justify-center px-8 pt-7 pb-9 z-0 group">
+                <div class="absolute left-0 top-0 w-full h-full -z-10 after:bg-gradient-to-b after:from-orange-500 after:to-orange-400 after:absolute after:w-full after:h-0 after:left-0 after:top-full group-hover:after:top-0 group-hover:after:h-full after:transition-all after:shadow-md after:shadow-transparent group-hover:after:shadow-xl group-hover:after:shadow-orange-500 after:rounded-t-2xl"></div>
+                <div class="text-orange-400 group-hover:text-white z-10 transition drop-shadow-lg">
+                    <svg
+                        class="w-8 fill-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        ><path
+                            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59c.4.07.55-.17.55-.38c0-.19-.01-.82-.01-1.49c-2.01.37-2.53-.49-2.69-.94c-.09-.23-.48-.94-.82-1.13c-.28-.15-.68-.52-.01-.53c.63-.01 1.08.58 1.23.82c.72 1.21 1.87.87 2.33.66c.07-.52.28-.87.51-1.07c-1.78-.2-3.64-.89-3.64-3.95c0-.87.31-1.59.82-2.15c-.08-.2-.36-1.02.08-2.12c0 0 .67-.21 2.2.82c.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82c.44 1.1.16 1.92.08 2.12c.51.56.82 1.27.82 2.15c0 3.07-1.87 3.75-3.65 3.95c.29.25.54.73.54 1.48c0 1.07-.01 1.93-.01 2.2c0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
+                        ></path>
+                    </svg>
+                </div>
+            </a>
+            <a aria-label="Email Alpine" href="mailto:contact@alpinesoftworks.cc" class="relative flex w-12 h-8 items-center justify-center px-8 pt-7 pb-9 z-0 group">
+                <div class="absolute left-0 top-0 w-full h-full -z-10 after:bg-gradient-to-b after:from-orange-500 after:to-orange-400 after:absolute after:w-full after:h-0 after:left-0 after:top-full group-hover:after:top-0 group-hover:after:h-full after:transition-all after:shadow-md after:shadow-transparent group-hover:after:shadow-xl group-hover:after:shadow-orange-500 after:rounded-t-2xl"></div>
+                <div class="text-orange-400 group-hover:text-white z-10 transition drop-shadow-lg">
+                    <svg
+                        class="w-8 fill-white"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        ><path
+                            d="M22 8.608v8.142a3.25 3.25 0 0 1-3.066 3.245L18.75 20H5.25a3.25 3.25 0 0 1-3.245-3.066L2 16.75V8.608l9.652 5.056a.75.75 0 0 0 .696 0L22 8.608ZM5.25 4h13.5a3.25 3.25 0 0 1 3.234 2.924L12 12.154l-9.984-5.23a3.25 3.25 0 0 1 3.048-2.919L5.25 4h13.5-13.5Z"
+                        /></svg>
+                </div>
+            </a>
+        </div>
+    </footer>
+</div>
