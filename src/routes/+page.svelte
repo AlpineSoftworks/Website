49957f39ab6dfef82f6eec652798c5f3f670ea5b
Boris <145894897+0boris@users.noreply.github.com>
2025-03-19 13:09:58 +0000

    Added Project Showcase.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <div
     class="flex flex-col h-screen w-full bg-radial-[90%_80%_at_50%_105%] from-[#7d3e1f] to-black"
 >
-    <div class="flex-grow flex flex-col justify-center">
+    <div class="flex-grow flex flex-col justify-center items-center">
         <div class="flex justify-center text-white">
             <svg
-                class="fill-orange-500 w-52"
+                class="fill-orange-500 w-32 lg:w-52"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 25 24"
                 ><path
@@ -18,6 +18,109 @@
                 class="text-8xl font-bold pb-4 bg-gradient-to-br from-orange-400 to-red-500 inline-block text-transparent bg-clip-text"
             >
                 Alpine
+            </h1>
+        </div>
+        <div class="flex flex-col gap-2 text-white mt-2">
+            <div
+                class="flex justify-between px-2 text-lg font-bold lg:max-w-6xl lg:w-full"
+            >
+                <div class="flex flex-1">
+                    <svg
+                        class="w-6 fill-white mr-2"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        ><path
+                            d="M6.75 22a.75.75 0 0 1-.102-1.493l.102-.007h1.749v-2.498H4.25a2.25 2.25 0 0 1-2.245-2.096L2 15.752V5.25a2.25 2.25 0 0 1 2.096-2.245L4.25 3h15.499a2.25 2.25 0 0 1 2.245 2.096l.005.154v10.502a2.25 2.25 0 0 1-2.096 2.245l-.154.005h-4.25V20.5h1.751a.75.75 0 0 1 .102 1.494L17.25 22H6.75Zm7.248-3.998h-4l.001 2.498h4l-.001-2.498Z"
+                        /></svg
+                    >
+                    Desktop
+                </div>
+                <div class="flex flex-1">
+                    <svg
+                    class="w-6 fill-white mr-2"
+                    viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        ><path
+                            d="M8.904 16.5h6.192C14.476 19.773 13.234 22 12 22c-1.197 0-2.4-2.094-3.038-5.204l-.058-.294h6.192-6.192Zm-5.838.001H7.37c.365 2.082.983 3.854 1.793 5.093a10.029 10.029 0 0 1-5.952-4.814l-.146-.279Zm13.563 0h4.305a10.028 10.028 0 0 1-6.097 5.093c.755-1.158 1.344-2.778 1.715-4.681l.076-.412h4.306-4.306Zm.302-6.5h4.87a10.055 10.055 0 0 1-.257 5H16.84a28.539 28.539 0 0 0 .13-4.344L16.93 10h4.87-4.87ZM2.198 10h4.87a28.211 28.211 0 0 0 .034 4.42l.057.58H2.456a10.047 10.047 0 0 1-.258-5Zm6.377 0h6.85a25.838 25.838 0 0 1-.037 4.425l-.062.575H8.674a25.979 25.979 0 0 1-.132-4.512L8.575 10h6.85-6.85Zm6.37-7.424-.109-.17A10.027 10.027 0 0 1 21.372 8.5H16.78c-.316-2.416-.956-4.492-1.837-5.923l-.108-.17.108.17Zm-5.903-.133.122-.037C8.283 3.757 7.628 5.736 7.28 8.06l-.061.44H2.628a10.028 10.028 0 0 1 6.414-6.057l.122-.037-.122.037ZM12 2.002c1.319 0 2.646 2.542 3.214 6.183l.047.315H8.739C9.28 4.691 10.643 2.002 12 2.002Z"
+                        /></svg
+                    >
+                    Website
+                </div>
+            </div>
+
+            <div
+                class="grid gap-2 text-center mb-32 lg:max-w-6xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"
+            >
+                <a
+                    href="#"
+                    class="group rounded-lg border border-white/20 px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-white/10 hover:bg-white/10"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 class={`mb-3 text-2xl font-semibold`}>
+                        CrackerTorrent
+                        <span
+                            class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"
+                        >
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p class={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                        A fast, lightweight yet beautiful torrent client.
+                    </p>
+                </a>
+
+                <div
+                    class="group rounded-lg border border-white/20 px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-white/10 hover:bg-white/10 opacity-20"
+                >
+                    <h2 class={`mb-3 text-2xl font-semibold`}>
+                        Coming Soon
+                        <span
+                            class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"
+                        >
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p class={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                        More projects, on the way.
+                    </p>
+                </div>
+
+                <a
+                    href="https://termed.bio/"
+                    class="group rounded-lg border border-white/20 px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-white/10 hover:bg-white/10"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 class={`mb-3 text-2xl font-semibold`}>
+                        TermedBio
+                        <span
+                            class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"
+                        >
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p class={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                        A simple yet effective solution to losing your social media accounts.
+                    </p>
+                </a>
+
+                <div
+                    class="group rounded-lg border border-white/20 px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:border-white/10 hover:bg-white/10 opacity-20"
+                >
+                    <h2 class={`mb-3 text-2xl font-semibold`}>
+                        Coming Soon
+                        <span
+                            class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"
+                        >
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p class={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                        More projects, on the way.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -29,9 +132,17 @@
             precision.</span
         >
         <div class="flex justify-center mt-4">
-            <a aria-label="Alpine GitHub" href="https://github.com/AlpineSoftworks" class="relative flex w-12 h-8 items-center justify-center px-8 pt-7 pb-9 z-0 group">
-                <div class="absolute left-0 top-0 w-full h-full -z-10 after:bg-gradient-to-b after:from-orange-500 after:to-orange-400 after:absolute after:w-full after:h-0 after:left-0 after:top-full group-hover:after:top-0 group-hover:after:h-full after:transition-all after:shadow-md after:shadow-transparent group-hover:after:shadow-xl group-hover:after:shadow-orange-500 after:rounded-t-2xl"></div>
-                <div class="text-orange-400 group-hover:text-white z-10 transition drop-shadow-lg">
+            <a
+                aria-label="Alpine GitHub"
+                href="https://github.com/AlpineSoftworks"
+                class="relative flex w-12 h-8 items-center justify-center px-8 pt-7 pb-9 z-0 group"
+            >
+                <div
+                    class="absolute left-0 top-0 w-full h-full -z-10 after:bg-gradient-to-b after:from-orange-500 after:to-orange-400 after:absolute after:w-full after:h-0 after:left-0 after:top-full group-hover:after:top-0 group-hover:after:h-full after:transition-all after:shadow-md after:shadow-transparent group-hover:after:shadow-xl group-hover:after:shadow-orange-500 after:rounded-t-2xl"
+                ></div>
+                <div
+                    class="text-orange-400 group-hover:text-white z-10 transition drop-shadow-lg"
+                >
                     <svg
                         class="w-8 fill-white"
                         xmlns="http://www.w3.org/2000/svg"
@@ -42,16 +153,25 @@
                     </svg>
                 </div>
             </a>
-            <a aria-label="Email Alpine" href="mailto:contact@alpinesoftworks.cc" class="relative flex w-12 h-8 items-center justify-center px-8 pt-7 pb-9 z-0 group">
-                <div class="absolute left-0 top-0 w-full h-full -z-10 after:bg-gradient-to-b after:from-orange-500 after:to-orange-400 after:absolute after:w-full after:h-0 after:left-0 after:top-full group-hover:after:top-0 group-hover:after:h-full after:transition-all after:shadow-md after:shadow-transparent group-hover:after:shadow-xl group-hover:after:shadow-orange-500 after:rounded-t-2xl"></div>
-                <div class="text-orange-400 group-hover:text-white z-10 transition drop-shadow-lg">
+            <a
+                aria-label="Email Alpine"
+                href="mailto:contact@alpinesoftworks.cc"
+                class="relative flex w-12 h-8 items-center justify-center px-8 pt-7 pb-9 z-0 group"
+            >
+                <div
+                    class="absolute left-0 top-0 w-full h-full -z-10 after:bg-gradient-to-b after:from-orange-500 after:to-orange-400 after:absolute after:w-full after:h-0 after:left-0 after:top-full group-hover:after:top-0 group-hover:after:h-full after:transition-all after:shadow-md after:shadow-transparent group-hover:after:shadow-xl group-hover:after:shadow-orange-500 after:rounded-t-2xl"
+                ></div>
+                <div
+                    class="text-orange-400 group-hover:text-white z-10 transition drop-shadow-lg"
+                >
                     <svg
                         class="w-8 fill-white"
                         viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                         ><path
                             d="M22 8.608v8.142a3.25 3.25 0 0 1-3.066 3.245L18.75 20H5.25a3.25 3.25 0 0 1-3.245-3.066L2 16.75V8.608l9.652 5.056a.75.75 0 0 0 .696 0L22 8.608ZM5.25 4h13.5a3.25 3.25 0 0 1 3.234 2.924L12 12.154l-9.984-5.23a3.25 3.25 0 0 1 3.048-2.919L5.25 4h13.5-13.5Z"
-                        /></svg>
+                        /></svg
+                    >
                 </div>
             </a>
         </div>
